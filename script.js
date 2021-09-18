75288febbd3161b7a3bb21d7e6afb1c7cef39824
Parbat Limbu <50946258+Habray@users.noreply.github.com>
2021-09-18 20:36:31 +0545

    Color Changer Website

---
+++ script.js
@@ -0,0 +1,39 @@
+function changeColorBlue(){
+    document.body.style.backgroundColor = "#0d6efd";
+    document.getElementById("title").style.color = "black";
+}
+
+function changeColorGray(){
+    document.body.style.backgroundColor = "#6c757d";
+    document.getElementById("title").style.color = "black";
+}
+
+function changeColorGreen(){
+    document.body.style.backgroundColor = "#198754";
+    document.getElementById("title").style.color = "black";
+}
+
+function changeColorRed(){
+    document.body.style.backgroundColor = "#dc3545";
+    document.getElementById("title").style.color = "black";
+}
+
+function changeColorYellow(){
+    document.body.style.backgroundColor = "#ffc107";
+    document.getElementById("title").style.color = "black";
+}
+
+function changeColorLightBlue(){
+    document.body.style.backgroundColor = "#0dcaf0";
+    document.getElementById("title").style.color = "black";
+}
+
+function changeColorWhite(){
+    document.body.style.backgroundColor = "#f8f9fa";
+    document.getElementById("title").style.color = "black";
+}
+
+function changeColorBlack(){
+    document.body.style.backgroundColor = "#212529";
+    document.getElementById("title").style.color = "white";
+}
